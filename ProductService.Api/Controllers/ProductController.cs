7023848d0c61ad925a1b33eac951d26d6c26e5ab
Krivyh.a <Krivyh.a@medindex.ru>
2022-07-12 00:20:59 +0300

    prettify swagger

--- ProductService.Api/Controllers/ProductController.cs
+++ ProductService.Api/Controllers/ProductController.cs
@@ -1,21 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
 using ProductService.Api.Models;
 
 namespace ProductService.Api.Controllers;
 
-[Route("product")]
+/// <summary>
+/// CRUD for products 
+/// </summary>
 [ApiController]
+[Route("product")]
+[ApiVersion("1")]
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
 public class ProductController : ControllerBase
 {
     private readonly ProductRepository _repository;
 
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="repository">Repository of products</param>
     public ProductController(ProductRepository repository)
     {
         _repository = repository;
     }
 
-    [HttpGet]
-    public IActionResult Get([FromQuery] string productName)
+    /// <summary>
+    /// Get product by name
+    /// </summary>
+    /// <param name="productName">Product name</param>
+    /// <response code="200">Product</response>
+    /// <response code="404">Product not found</response>
+    [HttpGet("{productName}", Name = "Get")]
+    [ProducesResponseType(typeof(Product), 200)]
+    [ProducesResponseType(typeof(ProblemDetails), 404)]
+    public IActionResult Get([FromRoute]  [Required] [MaxLength(100)] string productName)
     {
         if (!_repository.Products.TryGetValue(productName, out var product))
         {
@@ -24,33 +44,63 @@ public class ProductController : ControllerBase
         return Ok(product);
     }
 
-    [HttpPost]
+    /// <summary>
+    /// Create product
+    /// </summary>
+    /// <param name="product">Product to create</param>
+    /// <response code="201">Product create</response>
+    /// <response code="409">Product with the same name already exist</response>
+    [HttpPost(Name = "Create")]
+    [ProducesResponseType(typeof(Product), 201)]
+    [ProducesResponseType(typeof(ProblemDetails), 409)]
     public IActionResult CreateProduct([FromBody] Product product)
     {
-        if (!_repository.Products.TryGetValue(product.Name, out _))
+        if (_repository.Products.TryGetValue(product.Name, out _))
         {
             return Conflict();
         }
 
         _repository.Products[product.Name] = product;
         
-        return CreatedAtAction(nameof(Get), product.Name, product);
+        return CreatedAtAction(nameof(Get),new {productName = product.Name}, product);
     }
 
-    [HttpPut]
-    public IActionResult UpdateProduct([FromBody] Product product)
+    /// <summary>
+    /// Update product
+    /// </summary>
+    /// <param name="productName">Product name</param>
+    /// <param name="product">Product to update</param>
+    /// <response code="204">Product updated</response>
+    /// <response code="404">Product not found</response>
+    /// <response code="400">Bad request</response>
+    [HttpPut("{productName}", Name = "Update")]
+    [ProducesResponseType(typeof(ProblemDetails), 400)]
+    [ProducesResponseType(typeof(ProblemDetails), 404)]
+    public IActionResult UpdateProduct([FromRoute] [Required] [MaxLength(100)] string productName, [FromBody] Product product)
     {
-        if (!_repository.Products.TryGetValue(product.Name, out _))
+        if (productName != product.Name)
+        {
+            return BadRequest();
+        }
+        
+        if (!_repository.Products.TryGetValue(productName, out _))
         {
             return NotFound();
         }
 
-        _repository.Products[product.Name] = product;
+        _repository.Products[productName] = product;
         return NoContent();
     }
 
-    [HttpDelete]
-    public IActionResult DeleteProduct([FromQuery] string productName)
+    /// <summary>
+    /// Delete product
+    /// </summary>
+    /// <param name="productName">Product name to delete</param>
+    /// <response code="204">Product updated</response>
+    /// <response code="404">Product not found</response>
+    [HttpDelete("{productName}", Name = "Delete")]
+    [ProducesResponseType(typeof(ProblemDetails), 404)]
+    public IActionResult DeleteProduct([FromRoute] [Required] [MaxLength(100)] string productName)
     {
         if (!_repository.Products.TryGetValue(productName, out _))
         {
