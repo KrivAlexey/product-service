99b831d6ff5f5d3388067a218fe4f9f24dc53849
Krivyh.a <Krivyh.a@medindex.ru>
2022-07-11 22:12:27 +0300

    ProductController.cs

---
+++ ProductService.Api/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Api.Controllers;
+
+[Route("product")]
+public class ProductController : ControllerBase
+{
+    public IActionResult Get()
+    {
+        return Ok("adsd");
+    }
+}
--- ProductService.Api/Program.cs
+++ ProductService.Api/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+ConfigureServices(builder.Services);
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+ConfigureApp(app);
 app.Run();
+
+static void ConfigureServices(IServiceCollection services)
+{
+    var mvcBuilder = services.AddControllers();
+    mvcBuilder.AddControllersAsServices();
+}
+
+static void ConfigureApp(WebApplication app)
+{
+    app.MapControllers();
+}
